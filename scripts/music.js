928e6d6e11f10b6cfdc25f540912074785934abf
Hassan Salex <7166704@isd535.org>
2022-05-17 11:19:23 -0500

    Commit - Fix Home and Featured

---
+++ scripts/music.js
@@ -0,0 +1,16 @@
+const music = document.querySelector('#music');
+
+
+let audio = new Audio();
+audio.src = '/sound/NGGUP.mp3';
+
+music.addEventListener('click', function(){
+    audio.play();
+    audio.loop = true;
+});
+
+
+const buttonmeme = document.getElementById('meme')
+let meme = buttonmeme.getAttribute("class")
+
+buttonmeme.setAttribute("type", "button")
