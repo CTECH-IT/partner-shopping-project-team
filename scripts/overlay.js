d7a5174f379d68fa5fe1cd075c4ce4611a401153
Hassan Salex <7166704@isd535.org>
2022-05-25 11:31:36 -0500

    Commit - Cart need fixing

--- scripts/overlay.js
+++ scripts/overlay.js
@@ -1,8 +1,37 @@
-const cart = document.querySelector('cart')
-const cartDisplay = document.querySelector('cart_overlay')
-const buttonTrigger = document.getElementById('button')
+const cart = document.querySelector('.cart');
+const cartDisplay = document.querySelector('.cart_overlay');
+const buttonTrigger = document.getElementById('button');
+var IscartShowing = false
+const navBar = document.querySelector('.navbar_container')
+const navBar1 = document.querySelector('.navbar')
 
+function Transition() {
 
-function CartTransition() {
-    document.querySelector('cart_overlay').cssText = 'visibility: visible;'
+    if (IscartShowing == false) {
+        navBar1.classList.add('hideNav');
+        navBar.classList.add('hideNav');
+        buttonTrigger.classList.remove('buttonRemove');
+        cart.classList.add('showCart');
+        cartDisplay.classList.add('transparentBcg');
+        buttonTrigger.classList.add('buttonMove');
+        IscartShowing = true
+    }
+    else if (IscartShowing == true) {
+        navBar1.classList.remove('hideNav');
+        navBar.classList.remove('hideNav');
+        cart.classList.remove('showCart');
+        cartDisplay.classList.remove('transparentBcg');
+        buttonTrigger.classList.remove('buttonMove');
+        buttonTrigger.classList.add('buttonRemove');
+
+        IscartShowing = false
+    }
 }
+
+
+
+buttonTrigger.addEventListener('click', Transition)
+
+
+
+
