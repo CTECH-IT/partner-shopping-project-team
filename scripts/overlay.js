f84b449e428d368b8611e7eb97d72bb96bda425d
ShadowPowerRift <7166704@isd535.org>
2022-05-24 19:42:55 -0500

    Commit - fix javascript

--- scripts/overlay.js
+++ scripts/overlay.js
@@ -3,6 +3,6 @@ const cartDisplay = document.querySelector('cart_overlay')
 const buttonTrigger = document.getElementById('button')
 
 
-document.querySelector('#button').addEventListener('click', () => {
- document.querySelector('.cart').classList.add('cartTransform');
-}); 
+function CartTransition() {
+    document.querySelector('cart_overlay').cssText = 'visibility: visible;'
+}
