97b5c390f732256d4dd32b3cdd3da781e298be4e
ShadowPowerRift <7166704@isd535.org>
2022-05-27 01:46:20 -0500

    Commit - Finish Form Handler

---
+++ scripts/formhandler.js
@@ -0,0 +1,11 @@
+(function (window) {
+    'use strict';
+    let App = window.app || {};
+
+    function Formhandler() {
+        
+    }
+
+    App.Formhandler = Formhandler;
+    window.app = App;
+})(window);
