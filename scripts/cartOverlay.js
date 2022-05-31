8ba0e36431c4fa2d84cb6e7dcc6fcd08e7e6da7f
ShadowPowerRift <7166704@isd535.org>
2022-05-30 19:46:14 -0500

    Commit - Cart Overlay Add Items Feature Work On

---
+++ scripts/cartOverlay.js
@@ -0,0 +1,12 @@
+const deleteButton = document.querySelector('.fa-trash');
+const addButton = document.querySelector()
+
+function deleteItem (){
+    deleteButton.parentElement.remove();
+}
+
+function addItem(){
+
+}
+
+deleteButton.addEventListener('click', deleteItem);
